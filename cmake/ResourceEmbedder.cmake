709e0bdea51a620b47aad3fb03985e1a7d35748f
innerviewer <innerviewerr@gmail.com>
2024-04-01 00:15:14 +0200

    (CMake) Fixed EmbedResource dependency order.

--- cmake/ResourceEmbedder.cmake
+++ cmake/ResourceEmbedder.cmake
@@ -2,7 +2,7 @@
 ## add_dependencies() : add EmbedResourcesTarget to your project.
 ## target_include_directories() : include the path to the ${CMAKE_CURRENT_BINARY_DIR}/Utils/include/Utils/EmbedResources.
 function(EmbedResources EXPORT_DIRECTORY WORKING_DIRECTORY EMBED_RESOURCES_LIST)
-    add_custom_target(EmbedResourcesTarget ALL
+    add_custom_target(EmbedResourcesTarget
             COMMAND python ResourceEmbedder.py --working-directory "${WORKING_DIRECTORY}" --export-directory ${EXPORT_DIRECTORY} --resources "\"${EMBED_RESOURCES_LIST}\""
             WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/Engine/Core/libs/Utils/py")
 endfunction()
