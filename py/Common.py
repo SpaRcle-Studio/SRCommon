4b101dc731d6ca02e4eeb6be1d3cd28d7c2517f0
innerviewer <innerviewerr@gmail.com>
2024-04-06 14:50:10 +0200

    (CMake) Fixed build.

--- py/Common.py
+++ py/Common.py
@@ -6,28 +6,28 @@ import typing
 
 try:
     import numpy as np
-except ImportError:
+except Exception as e:
     subprocess.run([sys.executable, 'pip', 'install', 'numpy'])
     subprocess.run([sys.executable, '-m', 'pip', 'install', 'numpy'])
     import numpy as np
 
 try:
     import argparse
-except ImportError:
+except Exception as e:
     subprocess.run([sys.executable, 'pip', 'install', 'argparse'])
     subprocess.run([sys.executable, '-m', 'pip', 'install', 'argparse'])
     import argparse
 
 try:
     import xml.etree.ElementTree as ET
-except ImportError:
+except Exception as e:
     subprocess.run([sys.executable, 'pip', 'install', 'elementtree'])
     subprocess.run([sys.executable, '-m', 'pip', 'install', 'elementtree'])
     import xml.etree.ElementTree as ET
 
 try:
     import clang
-except ImportError:
+except Exception as e:
     subprocess.run([sys.executable, 'pip', 'install', 'clang'])
     subprocess.run([sys.executable, '-m', 'pip', 'install', 'clang'])
     import clang
