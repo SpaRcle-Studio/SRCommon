91aeca59b08e0b1d0fc0b3f6dd418ad1049c81a9
Monika0000 <rotaru5craft@gmail.com>
2025-07-10 11:56:03 +0100

    (Linux) Fixed compilation under fucking GCC.

--- inc/Utils/Math/AABB.h
+++ inc/Utils/Math/AABB.h
@@ -9,15 +9,8 @@
 
 namespace SR_MATH_NS {
     struct SR_RENDERER_DLL_API AABB {
-        union {
-            FVector3 min = FVector3(0.f, 0.f, 0.f);
-            FVector3 position; ///< Alias for min
-        };
-
-        union {
-            FVector3 max = FVector3(0.f, 0.f, 0.f);
-            FVector3 size; ///< Alias for max
-        };
+        FVector3 min = FVector3(0.f, 0.f, 0.f);
+        FVector3 max = FVector3(0.f, 0.f, 0.f);
 
         AABB();
         AABB(const FVector3& min, const FVector3& max);
